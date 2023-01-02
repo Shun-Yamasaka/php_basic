d1c8072d004c69c8dad0fb00b95c4d4e1fd10057
shun yamasaka <118syuunico@gmail.com>
2023-01-02 10:04:50 +0900

    配列の配列

---
+++ array_array.php
@@ -0,0 +1,9 @@
+<!-- 配列の配列 -->
+<?php
+    $class1 = array("はるき", "かおる", "ひでと");
+    $class2 = array("ゆきこ", "ゆか", "まなみ");
+    $students = array($class1, $class2);
+    var_dump($class1);
+    var_dump($class2);
+    var_dump($students);
+?>
